720c3573f3bb4adca3995eeca71a979050bc01f7
Isaac Kim <myong328@gmail.com>
2018-11-26 10:23:25 +0900

    initial setup

---
+++ fakeBnB/fakeBnB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fakeBnB
+//
+//  Created by Isaac Kim on 26/11/2018.
+//  Copyright © 2018 Isaac Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
